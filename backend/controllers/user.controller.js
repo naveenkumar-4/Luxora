3443ae1426feda5f159d6a65ba627b88822e1a58
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-07-25 21:22:07 +0530

    created user login

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -18,3 +18,29 @@ export const createUser = asyncHandler(async (req, res, next) => {
     next(err);
   }
 });
+
+export const loginUser = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    // checking user if exist or not
+    if (!email && !password) {
+      throw new Error("All fields are mandatory");
+    }
+    if (!email || email.trim() == "") {
+      throw new Error("Email is required");
+    }
+    if (!password || password == "") {
+      throw new Error("Password is required");
+    }
+    const findUser = await UserSchema.findOne({ email });
+    if (findUser && (await findUser.isPasswordMatched(password))) {
+      res.status(200).json(findUser);
+    } else {
+      // console.log("Login failed");
+      throw new Error("Invalid Credentials");
+    }
+  } catch (err) {
+    console.log(err.message);
+    next(err);
+  }
+});
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { createUser } from "../controllers/user.controller.js";
+import { createUser, loginUser } from "../controllers/user.controller.js";
 export const userRouter = express.Router();
 
 userRouter.post("/register", createUser);
+userRouter.post("/login", loginUser);
