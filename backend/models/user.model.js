428fc78696388d0bd05aa0cb72f94edd51a6b46d
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-07-25 05:45:15 +0530

    Created user model with mongoose

---
+++ backend/models/user.model.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose"; // Erase if already required
+
+// Declare the Schema of the Mongo model
+export const userSchema = new mongoose.Schema({
+    firstname:{
+        type:String,
+        required:true,
+    },
+    lastname:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    mobile:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+});
+
