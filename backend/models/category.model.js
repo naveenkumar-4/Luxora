e56c65bca2bffdff4ee9af1462ae4a45eeca12db
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-07-30 05:57:46 +0530

    added category.model.js

---
+++ backend/models/category.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+// Declare the Schema of the Mongo model
+var categorySchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+  },
+  { timestamps: true }
+);
+
+//Export the model
+export const CategorySchema = new mongoose.model("Category", categorySchema);
