97de6e2d725d4ce647d5f107cd59787e63dae0a6
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-08-02 06:32:30 +0530

    added order.model.js

---
+++ backend/models/order.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+// Declare the Schema of the Mongo model
+var orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        count: Number,
+        color: String,
+      },
+    ],
+    paymentIntent: {},
+    orderStatus: {
+      type: String,
+      default: "Not processed",
+      enum: [
+        "Not Processed",
+        "Cash On Delivery",
+        "Processing",
+        "Dispatched",
+        "Cancelled",
+        "Delivered",
+      ],
+    },
+    orderedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+//Export the model
+module.exports = mongoose.model("User", userSchema);
